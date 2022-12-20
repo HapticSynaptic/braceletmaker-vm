c58d9e72dde5f31b38c421f36e59c90e4724e8b0
frontendlane <petar@frontendlane.com>
2022-12-20 20:21:16 +0100

    1.6.0

--- src/util/scratch-link-websocket.js
+++ src/util/scratch-link-websocket.js
@@ -31,7 +31,7 @@ class ScratchLinkWebSocket {
             this._ws = new WebSocket('wss://127.0.0.1:20111/braceletmaker/bt');
             break;
         case 'SERIALPORT':
-            this._ws = new WebSocket('ws://127.0.0.1:20111/braceletmaker/serialport');
+            this._ws = new WebSocket('wss://127.0.0.1:20111/braceletmaker/serialport');
             break;
         default:
             throw new Error(`Unknown OpenblockLink socket Type: ${this._type}`);
